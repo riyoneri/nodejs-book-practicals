c4ff43ff69948abdb6f01f8db6b83a00b0c1bbe0
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-10 10:40:48 +0200

    chore: install selint-plugin-unicorn package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  eslintPluginUnicorn.configs["flat/recommended"],
+  {
+			rules: {
+				'unicorn/better-regex': 'error',
+        'unicorn/consistent-destructuring': 'error',
+        "unicorn/no-unused-properties": 'error'
+			},
+		},
 ];

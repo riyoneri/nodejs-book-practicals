6f6c1bc5911e0a90c1f16e07cf91938515759ff1
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-17 17:06:39 +0200

    feat: establish mysql connection in model [003]

--- 003 - movie-database-mysql/movie/model.mjs
+++ 003 - movie-database-mysql/movie/model.mjs
@@ -1,3 +1,14 @@
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "topSecret",
+  database: "movie-db",
+});
+
+await connection.connect();
+
 export const getAll = () => {};
 export const get = (_id) => {};
 export const remove = (_id) => {};

0b06855c73994a41fc7324e8b989d1d1c86752cb
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-20 11:57:57 +0200

    feat: create get model function [004]

--- 004 - movie-database-mongodb/movie/model.mjs
+++ 004 - movie-database-mongodb/movie/model.mjs
@@ -6,11 +6,11 @@ const Movie = mongoose.model("Movie", { title: String, year: Number });
 
 export const getAll = async () => Movie.find({});
 
+export const get = async (id) => Movie.findById(id);
+
 export const insert = async (_movie) => {};
 
 export const update = async (_movie) => {};
-
-export const get = async (_id) => {};
 
 export const remove = async (_id) => {};
 

28e0517dbef6a44b5a811b0f5f0cde5ee7faab34
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-20 10:05:17 +0200

    feat: create insert function in db [003]

--- 003 - movie-database-mysql/movie/model.mjs
+++ 003 - movie-database-mysql/movie/model.mjs
@@ -9,7 +9,23 @@ const connection = await mysql.createConnection({
 
 await connection.connect();
 
-export const getAll = () => {};
+export const getAll = async () => {
+  const [data] = await connection.query("SELECT * FROM Movies");
+
+  return data;
+};
+
+const insert = async (movie) => {
+  const [result] = await connection.query(
+    "INSERT INTO Movies (title, year) VALUES (?, ?)",
+    [movie.title, movie.year],
+  );
+
+  return { ...movie, id: result.insertId };
+};
+
 export const get = (_id) => {};
 export const remove = (_id) => {};
-export const save = (_movie) => {};
+export const save = (movie) => {
+  if (!movie.id) return insert(movie);
+};

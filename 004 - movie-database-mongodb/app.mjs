dd3b38d0e8491879b16215273ae019222b35d620
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-20 11:35:36 +0200

    feat: initial files [004]

---
+++ 004 - movie-database-mongodb/app.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import { createWriteStream } from "node:fs";
+
+import { router as movieRouter } from "./movie/index.mjs";
+
+const app = express();
+const accessLogStream = createWriteStream("access.log", { flags: "a" });
+
+app.use(morgan("dev", { immediate: true, stream: accessLogStream }));
+app.use(express.json());
+
+app.use("/movie", movieRouter);
+
+app.get("/", (_, response) => response.redirect("/movie"));
+
+app.listen(8080, () => {
+  // eslint-disable-next-line no-console
+  console.log("Server is listening to http://localhost:8080");
+});
---
+++ 004 - movie-database-mongodb/movie/model.mjs
@@ -0,0 +1,11 @@
+export const getAll = async () => {};
+
+export const insert = async (_movie) => {};
+
+export const update = async (_movie) => {};
+
+export const get = async (_id) => {};
+
+export const remove = async (_id) => {};
+
+export const save = (_movie) => {};

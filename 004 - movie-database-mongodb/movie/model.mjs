3b5c13078e6e9277dec074625ba4770f321757b0
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-20 11:56:56 +0200

    feat: create getAll module function [004]

--- 004 - movie-database-mongodb/movie/model.mjs
+++ 004 - movie-database-mongodb/movie/model.mjs
@@ -1,4 +1,10 @@
-export const getAll = async () => {};
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/moviedb");
+
+const Movie = mongoose.model("Movie", { title: String, year: Number });
+
+export const getAll = async () => Movie.find({});
 
 export const insert = async (_movie) => {};
 

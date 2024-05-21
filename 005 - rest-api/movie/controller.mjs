b4fd05856bbc519950404df9042a1cb94fbe0f39
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-21 15:43:21 +0200

    feat: error handling in controllers [005]

--- 005 - rest-api/movie/controller.mjs
+++ 005 - rest-api/movie/controller.mjs
@@ -3,22 +3,36 @@ import { Types } from "mongoose";
 import { get, getAll } from "./model.mjs";
 
 export const listAction = async (request, response) => {
-  const movies = await getAll(new Types.ObjectId("507f1f77bcf86cd799439011"));
+  try {
+    const movies = await getAll(new Types.ObjectId("507f1f77bcf86cd799439011"));
 
-  response.json({
-    movies,
-    links: [{ rel: "self", href: request.baseUrl + "/" }],
-  });
+    response.json({
+      movies,
+      links: [{ rel: "self", href: request.baseUrl + "/" }],
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    response.status(500).send("An error occured");
+  }
 };
 
 export const detailAction = async (request, response) => {
-  const movie = await get(
-    request.params.id,
-    new Types.ObjectId("507f1f77bcf86cd799439011"),
-  );
+  try {
+    const movie = await get(
+      request.params.id,
+      new Types.ObjectId("507f1f77bcf86cd799439011"),
+    );
 
-  response.json({
-    ...movie,
-    links: [{ rel: "self", href: `${request.baseUrl}/${movie?.id}` }],
-  });
+    if (!movie) return response.status(404).send("Not Found");
+
+    response.json({
+      ...movie,
+      links: [{ rel: "self", href: `${request.baseUrl}/${movie?.id}` }],
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    response.status(500).send("An error occured");
+  }
 };

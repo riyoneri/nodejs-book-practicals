b7cf29c6c0c19014e7d045e22cc381be1561770e
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-22 14:47:49 +0200

    feat: protect movies endpoints with expressjwt [005]

--- 005 - rest-api/app.mjs
+++ 005 - rest-api/app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import { expressjwt } from "express-jwt";
 import morgan from "morgan";
 import { createWriteStream } from "node:fs";
 
@@ -11,8 +12,19 @@ const accessLogStream = createWriteStream("access.log", { flags: "a" });
 app.use(morgan("dev", { immediate: true, stream: accessLogStream }));
 app.use(express.json());
 
-app.use("/movie", movieRouter);
+app.use(
+  "/movie",
+  expressjwt({ secret: "secret", algorithms: ["HS256"] }),
+  movieRouter,
+);
 app.use("/auth", authRouter);
+
+app.use((error, _request, response, next) => {
+  if (error.name === "UnauthorizedError")
+    return response.status(401).json("Unauthorized");
+
+  next();
+});
 
 app.get("/", (_, response) => response.redirect("/movie"));
 

db3d8ee113c09afa5d05bac0121d1f9ff0e612fd
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-22 14:13:04 +0200

    feat: implement login logic [005]

--- 005 - rest-api/auth/controller.mjs
+++ 005 - rest-api/auth/controller.mjs
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import { createHash } from "node:crypto";
 
 import User from "./model.mjs";
@@ -12,6 +13,25 @@ export const registerAction = async (request, response) => {
     const savedUser = await newUser.save();
 
     response.status(201).json(savedUser.format());
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    response.status(500).send("An error occured");
+  }
+};
+
+export const loginAction = async (request, response) => {
+  try {
+    const user = await User.findOne({
+      username: request.body.username,
+      password: createHash("md5").update(request.body.password).digest("hex"),
+    });
+
+    if (!user) return response.status(401).json("Unauthorized");
+
+    const token = jwt.sign({ id: user.format().id }, "secret", {});
+
+    response.json({ token });
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error);
--- 005 - rest-api/auth/index.mjs
+++ 005 - rest-api/auth/index.mjs
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { registerAction } from "./controller.mjs";
+import { loginAction, registerAction } from "./controller.mjs";
 
 const router = Router();
 
 router.post("/register", registerAction);
+router.post("/login", loginAction);
 
 export { router };

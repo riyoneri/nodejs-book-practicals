553fee4679afbef230cef2270b08cebcdad9a4b2
Lionel Kaneza <lionkaneza@gmail.com>
2024-05-22 12:23:02 +0200

    feat: create auth routes [005]

--- 005 - rest-api/app.mjs
+++ 005 - rest-api/app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import { createWriteStream } from "node:fs";
 
+import { router as authRouter } from "./auth/index.mjs";
 import { router as movieRouter } from "./movie/index.mjs";
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(morgan("dev", { immediate: true, stream: accessLogStream }));
 app.use(express.json());
 
 app.use("/movie", movieRouter);
+app.use("/auth", authRouter);
 
 app.get("/", (_, response) => response.redirect("/movie"));
 
